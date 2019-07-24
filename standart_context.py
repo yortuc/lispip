5dafe42bfb16385845d79029ed9865b28e85cf87
Evren Yortuçboylu <yortucboylu@gmail.com>
2019-07-24 09:25:23 +0200

    Update standart_context.py

--- standart_context.py
+++ standart_context.py
@@ -1,12 +1,18 @@
+import random
+
 def extend_context(c1):
     for k in c1:
         ctx[k] = c1[k]
 
 def set_global_var(key, value):
-        ctx[key] = value
+    ctx[key] = value
 
+def identity(*params):
+    pass
+        
 ctx = {
-    'set': lambda k, v: set_global_var,
+    'block': identity,
+    'set': lambda k, v: set_global_var(key, value),
 
     # string funcs
     'string': lambda *x: ' '.join(x),
@@ -24,6 +30,8 @@ ctx = {
     '>=': lambda x, y: x>=y,
     '<=': lambda x, y: x<=y,
     'or': lambda x, y: x or y,
+    'int': lambda str: int(str),
+    'create-random': lambda x, y: random.randint(x, y),
 
     # List funcs
     'list': lambda *x: [k for k in x],
@@ -31,7 +39,11 @@ ctx = {
     'first': lambda lst: lst[0],
     'tail': lambda lst: lst[1:],
     'map': lambda fn, lst: [fn(k) for k in lst],
+    'list-add': lambda lst, item: lst + [item],
     
     # import python-binding
-    'import': lambda file_name: extend_context(getattr(__import__(file_name), 'ctx'))
+    'import': lambda file_name: extend_context(getattr(__import__(file_name), 'ctx')),
+    
+     # meta
+    'running-context': lambda: [c for c in ctx],
 }
